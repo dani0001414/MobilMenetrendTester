845a9224c37b41122cfaa40309fcd27efb3028f0
dani0001414 <dani0001414@gmail.com>
2019-04-05 13:39:29 +0200

    Update streamerdata.js

--- streamerdata.js
+++ streamerdata.js
@@ -10,5 +10,5 @@ var offlinePic = "https://i.imgur.com/5dZn6sc.png";
 //Offline állapotban megjelenő szöveg
 var offlineText = "Csak ezt a szöveget módosítsd!<br><span style=\"cursor: pointer; color: grey; text-decoration: underline;\" onclick=\"OfflineSite()\">OFFLINE MENETREND MEGTEKINTÉSE</span></span>"; 
 //Üres menetrendkor megjelenő szöveg
-var noEventsText = "Jelenleg nincs egy stream sem a menetrendben! Elszívták az UTP-vel együtt! <img src=\"http://static-cdn.jtvnw.net/emoticons/v1/25/1.0\" alt=\"23\"><br>Hamarosan újabb szálítmány!";
+var noEventsText = "Menetrend Jelenleg üres!";
 
