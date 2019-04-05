e01d06ab58ff855456ad33db5a64cefa5e4619cc
dani0001414 <dani0001414@gmail.com>
2019-04-05 15:12:16 +0200

    test

--- streamerdata.js
+++ streamerdata.js
@@ -1,12 +1,12 @@
 /**MobilBarát Menetrend Testreszabása. */
 /*Streamer adatok megadása*/
 
-//Csatorna pontos neve kis betűkkel.
-var streamer = "gige";
+//A twitch neved amivel a csatorna elérhető --> http://twitch.tv/twitchneved.
+var streamer = "avid_is_odd";
 //Kép megjelenítése amikor üres a menetrend.
-var noEventsPic = "https://i.imgur.com/5dZn6sc.png";
+var noEventsPic = "https://peldakep.hu/kep.png";
 //Kép megjelenítése amikor offline a menetrend(nembiztos, hogy megjelenik majd offline állapotban)
-var offlinePic = "https://i.imgur.com/5dZn6sc.png";
+var offlinePic = "https://peldakep.hu/kep.png";
 //Offline állapotban megjelenő szöveg
 var offlineText = "Csak ezt a szöveget módosítsd!<br><span style=\"cursor: pointer; color: grey; text-decoration: underline;\" onclick=\"OfflineSite()\">OFFLINE MENETREND MEGTEKINTÉSE</span></span>"; 
 //Üres menetrendkor megjelenő szöveg
