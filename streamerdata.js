448b405dbfa822d2c3e99c57a38f01c43fcd9efb
dani0001414 <dani0001414@gmail.com>
2019-04-09 19:28:41 +0200

    Oldal Feltöltés

---
+++ streamerdata.js
@@ -0,0 +1,14 @@
+/**MobilBarát Menetrend Testreszabása. */
+/*Streamer adatok megadása*/
+
+//A twitch neved amivel a csatorna elérhető --> http://twitch.tv/danx27.
+var streamer = "danx27";
+//Kép megjelenítése amikor üres a menetrend.
+var noEventsPic = "icon.png";
+//Kép megjelenítése amikor offline a menetrend(nembiztos, hogy megjelenik majd offline állapotban)
+var offlinePic = "icon.png";
+//Offline állapotban megjelenő szöveg
+var offlineText = "Nincs internet!"; 
+//Üres menetrendkor megjelenő szöveg
+var noEventsText = "Menetrend Jelenleg üres!";
+
