2afc34644f87e3e464dbef47b519e202b93bab5b
dani0001414 <dani0001414@gmail.com>
2019-04-15 21:46:19 +0200

    Update streamer_sw.js

--- streamer_sw.js
+++ streamer_sw.js
@@ -1,19 +1,7 @@
 const cacheName = 'v1';
-const cacheAssets = [
-  '/',
-  'index.html'
-]
+
 self.addEventListener('install', e => {
   console.log("Service Worker: Telepítve!");
-  e.waitUntil(
-    caches
-    .open(cacheName)
-    .then(cache => {
-      console.log("Service Worker: Gyorsítótárazás");
-      cache.addAll(cacheAssets);
-    })
-    .then(() => self.skipWaiting())
-  );
 });
 
 self.addEventListener('activate', e => {
