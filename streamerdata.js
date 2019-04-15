e07eefe5df9af5fb7af9870f1285dd703926b8bc
dani0001414 <dani0001414@gmail.com>
2019-04-15 21:41:55 +0200

    Update streamerdata.js

--- streamerdata.js
+++ streamerdata.js
@@ -2,7 +2,7 @@
 /*Streamer adatok megadása*/
 
 //A twitch neved amivel a csatorna elérhető --> http://twitch.tv/twitchneved.
-var streamer = "danx27";
+var streamer = "tanarur";
 //Kép megjelenítése amikor üres a menetrend.
 var noEventsPic = "icon_144.png";
 //Kép megjelenítése amikor offline a menetrend(nembiztos, hogy megjelenik majd offline állapotban)
