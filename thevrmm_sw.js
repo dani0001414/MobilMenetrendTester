abe6bcb00ba41b85b9f707023969704328a504d2
dani0001414 <dani0001414@gmail.com>
2019-04-15 02:20:43 +0200

    Update thevrmm_sw.js

--- thevrmm_sw.js
+++ thevrmm_sw.js
@@ -1,5 +1,18 @@
+const cacheName = 'v1';
+const cacheAssets = [
+  'index.html',
+]
 self.addEventListener('install', e => {
   console.log("Service Worker: Telepítve!");
+  e.waitUntil(
+    caches
+    .open(cacheName)
+    .then(cache => {
+      console.log("Service Worker: Gyorsítótárazás");
+      cache.addAll(chacheAssets);
+    })
+    .then(() => self.skipWaiting())
+  );
 });
 
 self.addEventListener('activate', e => {
